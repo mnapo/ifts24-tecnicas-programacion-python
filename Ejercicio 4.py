9bd1ea523a949d85b4af3601200fec340eab49f1
napo <napolimatiase@gmail.com>
2025-05-16 23:53:25 -0300

    reemplazar con versión con while, try-except, continue y break

--- Ejercicio 4.py
+++ Ejercicio 4.py
@@ -2,30 +2,44 @@
 Se pide obtener que tipo de triángulo es.
 Sabiendo que ingresan 3 datos correspondientes al largo de cada lado.
 Recordar: el triángulo equilátero tiene los 3 lados iguales, el isósceles 2 lados iguales y el escaleno los 3 lados distintos.
+Versión con funciones: https://github.com/mnapo/ifts24-tecnicas-programacion-python2/blob/main/Ejercicio%2012.py
 '''
 
-def tipo_triangulo(lado1, lado2, lado3):
-  if lado1==lado2 and lado2==lado3:
-    return "equilátero"
-  elif lado1==lado2 or lado1==lado3 or lado2==lado3:
-    return "isóceles"
-  else:
-    return "escaleno"
+lado1 = 0
+lado2 = 0
+lado3 = 0
 
-def ingresar_lado():
-  lado = 0
-  while True:
-    try:
-      lado=float(input("Ingrese la medida del lado:"))
-      if lado<=0:
-        print("La medida del lado debe ser positiva, reingresar")
-        continue
-    except ValueError:
-      print("La medida del lado es incorrecta, reingresar")
+while True:
+  try:
+    lado1=float(input("Ingrese la medida del lado:"))
+    if lado1<=0:
+      print("La medida del lado debe ser positiva, reingresar")
       continue
-    return lado
+  except ValueError:
+    print("La medida del lado es incorrecta, reingresar")
+    continue
+while True:
+  try:
+    lado2=float(input("Ingrese la medida del lado:"))
+    if lado2<=0:
+      print("La medida del lado debe ser positiva, reingresar")
+      continue
+  except ValueError:
+    print("La medida del lado es incorrecta, reingresar")
+    continue
+while True:
+  try:
+    lado3=float(input("Ingrese la medida del lado:"))
+    if lado3<=0:
+      print("La medida del lado debe ser positiva, reingresar")
+      continue
+  except ValueError:
+    print("La medida del lado es incorrecta, reingresar")
+    continue
 
-lado1 = ingresar_lado()
-lado2 = ingresar_lado()
-lado3 = ingresar_lado()
-print("El triángulo es", tipo_triangulo(lado1, lado2, lado3))
+if lado1==lado2 and lado2==lado3:
+  print("El triángulo es equilátero")
+elif lado1==lado2 or lado1==lado3 or lado2==lado3:
+  print("El triángulo es isóceles")
+else:
+  print("El triángulo es escaleno")
